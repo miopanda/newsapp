62ffed6c8bb70a7dc657acd93645a3bd27de7171
miopanda <114249779+miopanda@users.noreply.github.com>
2023-08-03 17:44:32 +0900

    confirmメソッドの実装

--- app/javascript/revert_check.js
+++ app/javascript/revert_check.js
@@ -2,13 +2,19 @@ window.addEventListener('load', function(){
   const articleCheckButtons = document.querySelectorAll(".article-check-button");
   articleCheckButtons.forEach(function (button) {
     button.addEventListener('click', () => {
-      const articleID = button.getAttribute("data");
-      const XHR = new XMLHttpRequest();
-      XHR.open("GET", `checks/revert/${articleID}`, true);
-      XHR.send();
-      XHR.onload = () => {
-        button.remove()
+      const deleteFlg = window.confirm("記事の既読を削除しますか？");
+      if (deleteFlg) {
+        const articleID = button.getAttribute("data");
+        const XHR = new XMLHttpRequest();
+        XHR.open("GET", `checks/revert/${articleID}`, true);
+        XHR.send();
+        XHR.onload = () => {
+          button.remove()
+        }
+      } else {
+        ;
       }
+        
     });
   });
 });
